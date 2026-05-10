3dd0b8c44e67fed35beb1e4f929f129c3d88d3ca
Thomas Kain <tkain@stevens.edu>
2026-05-09 21:52:48 -0400

    Don't show login/sign-up buttons on home page when signed in
    Instead, show a button that takes the user to their main feed.

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,12 @@ import { Link } from "react-router-dom";
 import logo from "../assets/logo.svg";
 //UI IMPORTS//////////////////////////////////////
 import { Button, ButtonGroup, Box, AbsoluteCenter, Image } from "@chakra-ui/react";
+import AuthContext from "@/components/Auth/AuthContext.tsx";
+import { useContext } from "react";
 
 function Home() {
+  const [user] = useContext(AuthContext);
+
   return (
     <div>
       <Box>
@@ -23,13 +27,21 @@ function Home() {
             <h2>Track the games you've played. Discover what to play next.</h2>
 
             <ButtonGroup mt={12}>
-              <Link to={`/login`}>
-                <Button>Login</Button>
-              </Link>
+              {user ? (
+                <Link to={`/mainfeed`}>
+                  <Button>Go to Main Feed</Button>
+                </Link>
+              ) : (
+                <>
+                  <Link to={`/login`}>
+                    <Button>Login</Button>
+                  </Link>
 
-              <Link to={`/signup`}>
-                <Button>Sign Up</Button>
-              </Link>
+                  <Link to={`/signup`}>
+                    <Button>Sign Up</Button>
+                  </Link>
+                </>
+              )}
             </ButtonGroup>
           </Box>
         </AbsoluteCenter>
